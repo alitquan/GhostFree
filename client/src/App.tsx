8fc12d826bb9ebcaf9388462f234064bfe1a9270
alitquan <alitmallick.gmail.com>
2024-11-19 00:04:22 -0500

    added login page and potential testing in App.tsx. Neeed to implement the backend. Also need to implement Registration page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,6 +8,24 @@ import Login from '@pages/Login.tsx';
 import Register from '@pages/Register.tsx'; 
 
 function App() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+	  console.log("handleLogin() -- called");
+	  setIsLoggedIn(true);
+  };
+
+  window.onload = () => {
+	const token = localStorage.getItem('token');
+	if (token) { 
+		console.log("Token output: "); 
+		const userInfo = jwtDecode(token);
+		console.log(userInfo);
+		handleLogin();
+	}
+  }
+
   return (
     <HelmetProvider>
       <Router>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,59 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import styles from  '@styles/Home.module.css';
 
-const Login = () => {
+
+function Login  ({onLogin}) {
+
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const attemptLogin = async () => {
+		try { 
+			const response = await axios.posst('https://localhost:5000/login', {
+				username,
+				password,
+			});
+			localStorage.setItem('token', response.data.token);
+			onLogin();
+			console.log("attemptLogin() -- completed");
+		} catch (err) {
+			console.log(err);
+			setError(err.response?.data || 'Login failed'); 
+		}
+	}
+
+
 	return(
-		<div> 
-			Login Page	
-		</div>
+		<>
+			<div className={styles.containerLogin}>
+				<div className={`.container ${styles.signin}`}>
+				<h3>Login </h3> 
+
+				<div className={styles.signInField}>
+					<p> Username </p> 
+					<input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} />
+				</div>
+
+				<div className={styles.signInField}>
+					<p> Password </p> 
+					<input type="password"  value={password} onChange={(e)=>setPassword(e.target.value)} />
+				</div>
+
+
+				<div> 
+					{error && <p className={styles.loginError}>{error}</p>}
+				</div>
+
+
+				<button className={styles.loginButton} onClick={attemptLogin}> Sign In </button> 
+
+				</div>
+			</div>
+			 
+
+
+		</>
 	);
 } 
 
